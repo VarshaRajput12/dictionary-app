577e77a096d0a30cd7e7916796ad1067b59adb84
VarshaRajput12 <varsharajput353@gmail.com>
2023-03-16 22:50:28 +0530

    dictionary app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css"
+import { useEffect, useState } from "react";
+import { Container } from "@mui/material";
 
 function App() {
+  const [meanings, setMeanings] = useState([]);
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
+      );
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(meanings);
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:"100vh"}}>
+      <Container maxWidth="sm" backgroundColor="blue">hello</Container>
     </div>
   );
 }
